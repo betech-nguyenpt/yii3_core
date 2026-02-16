699c3f11b573bdf3a81408e2d54d773a242817ac
betech-nguyenpt <betech-nguyenpt@bisync.co.jp>
2026-02-17 04:37:56 +0700

    Add migration feature

---
+++ web/config/common/di/db-mysql.php
@@ -0,0 +1,19 @@
+<?php
+
+use Yiisoft\Db\Connection\ConnectionInterface;
+use Yiisoft\Db\Mysql\Connection;
+use Yiisoft\Db\Mysql\Driver;
+
+/** @var array $params */
+
+return [
+    ConnectionInterface::class => [
+        'class'     => Connection::class,
+        '__construct()' => [
+            'driver'    => new Driver(
+                $params['yiisoft/db-mysql']['dns'],
+                $params['yiisoft/db-mysql']['username'],
+                $params['yiisoft/db-mysql']['password']),
+        ],
+    ],
+];
--- web/config/common/params.php
+++ web/config/common/params.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Shared\ApplicationParams;
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Assets\AssetManager;
+use Yiisoft\Db\Mysql\Dsn;
 use Yiisoft\Definitions\Reference;
 use Yiisoft\Router\CurrentRoute;
 use Yiisoft\Router\UrlGeneratorInterface;
@@ -34,5 +35,16 @@ return [
         'injections' => [
             Reference::to(CsrfViewInjection::class),
         ],
+    ],
+
+    'yiisoft/db-mysql' => [
+        'dns'   => new Dsn('mysql', 'yii3_mysql', 'yii3_web', '3306'),
+        'username'  => 'root',
+        'password'  => 'root',
+    ],
+
+    'yiisoft/db-migration' => [
+        'newMigrationNamespace' => 'App\\Migration',
+        'sourceNamespaces'      => ['App\\Migration'],
     ],
 ];
