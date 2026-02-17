1c1155255b91cc5768fd2dd59375467df9c21c56
betech-nguyenpt <betech-nguyenpt@bisync.co.jp>
2026-02-17 21:36:51 +0700

    Authentication feature

--- web/src/Migration/M260217000000AdminUsers.php
+++ web/src/Migration/M260217000000AdminUsers.php
@@ -4,29 +4,32 @@ declare(strict_types=1);
 
 namespace App\Migration;
 
+use Yiisoft\Db\Expression\Expression;
 use Yiisoft\Db\Migration\MigrationBuilder;
 use Yiisoft\Db\Migration\RevertibleMigrationInterface;
+use Yiisoft\Db\Schema\Column\ColumnBuilder;
 
 final class M260217000000AdminUsers implements RevertibleMigrationInterface
 {
     public function up(MigrationBuilder $b): void
     {
         $b->createTable('{{%admin_users}}', [
-            'id' => $b->primaryKey(),
-            'username' => $b->string(),
-            'phone' => $b->string(),
-            'email' => $b->string(),
-            'password' => $b->text(),
-            'temp_pass' => $b->text(),
-            'fullname' => $b->string(),
-            'role_id' => $b->integer()->defaultValue(0)->comment('Id of role'),
-            'status' => $b->tinyInteger()->defaultValue(1)->comment('Status'),
-            'created_date' => $b->dateTime()->defaultExpression('current_timestamp()')->comment('Created date'),
-            'created_by' => $b->integer()->defaultValue(0)->comment('Created by'),
+            'id' => ColumnBuilder::primaryKey(),
+            'username' => ColumnBuilder::string(),
+            'phone' => ColumnBuilder::string(),
+            'email' => ColumnBuilder::string(),
+            'password' => ColumnBuilder::text(),
+            'temp_pass' => ColumnBuilder::text(),
+            'fullname' => ColumnBuilder::string(),
+            'role_id' => ColumnBuilder::integer()->defaultValue(0)->comment('Id of role'),
+            'status' => ColumnBuilder::tinyint()->defaultValue(1)->comment('Status'),
+            'created_date' => ColumnBuilder::datetime()->defaultValue(new Expression('current_timestamp()'))->comment('Created date'),
+            'created_by' => ColumnBuilder::integer()->defaultValue(0)->comment('Created by'),
         ]);
 
-        $b->createIndex('unique_username', '{{%admin_users}}', 'username', true);
+        $b->createIndex('{{%admin_users}}', 'unique_username', 'username', 'UNIQUE');
     }
+
 
     public function down(MigrationBuilder $b): void
     {
